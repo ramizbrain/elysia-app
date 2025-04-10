4e69b9954cbe5fe7048d0b91e1ff044879fadeaa
Ramiz Azhar <ramizbrain@gmail.com>
2025-04-10 16:11:35 +0700

    init

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .get("/ramiz", () => {
+    return {
+      name: "Ramiz",
+      age: 18,
+      hobbies: ["coding", "gaming", "reading"],
+      isMarried: true,
+      address: {
+        street: "123 Main St",
+        city: "Anytown",
+        state: "CA",
+      },
+    };
+  })
+
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
